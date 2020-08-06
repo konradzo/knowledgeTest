4461560f491d858c2c089dc64494a2fb21d0e2e0
Konrad Żochowski <konrad.zochowski@newspoint.pl>
2020-08-06 21:32:10 +0200

    handling categoryAlreadyExists and doesNotExist exceptions

--- src/main/java/pl/kzochowski/knowledgeTest/util/ControllerExceptionHandler.java
+++ src/main/java/pl/kzochowski/knowledgeTest/util/ControllerExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import pl.kzochowski.knowledgeTest.service.CategoryService;
 import pl.kzochowski.knowledgeTest.service.UserService;
 
 @ControllerAdvice
@@ -14,8 +15,24 @@ public class ControllerExceptionHandler {
 
     @ResponseBody
     @ExceptionHandler(UserService.UserAlreadyExistsException.class)
-    @ResponseStatus(HttpStatus.OK)
+    @ResponseStatus(HttpStatus.CONFLICT)
     String userAlreadyExists(UserService.UserAlreadyExistsException exception) {
+        exception.printStackTrace();
+        return exception.getMessage();
+    }
+
+    @ResponseBody
+    @ExceptionHandler(CategoryService.CategoryDoesNotExistException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String categoryDoesNotExist(CategoryService.CategoryDoesNotExistException exception) {
+        exception.printStackTrace();
+        return exception.getMessage();
+    }
+
+    @ResponseBody
+    @ExceptionHandler(CategoryService.CategoryAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    String categoryAlreadyExist(CategoryService.CategoryAlreadyExistsException exception) {
         exception.printStackTrace();
         return exception.getMessage();
     }
