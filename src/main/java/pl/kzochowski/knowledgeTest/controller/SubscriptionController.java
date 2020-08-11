be4d7aca26126dee7934dc67e189e57b5662438c
Konrad Żochowski <konrad.zochowski@newspoint.pl>
2020-08-11 23:07:05 +0200

    response statuses - subscriptionController

--- src/main/java/pl/kzochowski/knowledgeTest/controller/SubscriptionController.java
+++ src/main/java/pl/kzochowski/knowledgeTest/controller/SubscriptionController.java
@@ -2,10 +2,8 @@ package pl.kzochowski.knowledgeTest.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import pl.kzochowski.knowledgeTest.model.Subscription;
 import pl.kzochowski.knowledgeTest.model.SubscriptionList;
 import pl.kzochowski.knowledgeTest.service.SubscriptionService;
@@ -20,15 +18,16 @@ public class SubscriptionController {
     private final SubscriptionService subscriptionService;
 
     @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
     Subscription fetchSubscription(@PathVariable Integer id) {
         return subscriptionService.fetchSubscriptionById(id);
     }
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     SubscriptionList listSubscriptions() {
         List<Subscription> subscriptions = subscriptionService.listAllSubscriptions();
         log.info("Listing subscriptions list, size: {}", subscriptions.size());
         return new SubscriptionList(subscriptions);
     }
-
 }
